9fd0e59f63f5b4c43e58b610352edefe37b29c55
Marcel Claramunt Bassegoda <marcel.claramunt@estudiantat.upc.edu>
2022-10-27 11:40:30 +0200

    Create TFIDF.java
    Computes TFIDF vectors

---
+++ FONTS/vectorialmodel/TFIDF.java
@@ -0,0 +1,23 @@
+package vectorialmodel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import helpers.Maths;
+
+public class TFIDF {
+
+    public static HashMap<String, Double> computeTFIDF(HashMap<String, Integer> StringFrequencies, int maxFrequency, long documentsCount) {
+        Function<Integer, Double> tfidf = frequency -> (frequency / (double) maxFrequency) * Maths.log2(documentsCount / maxFrequency);
+        
+        return StringFrequencies.entrySet().stream()
+            .collect(Collectors.toMap(
+                Map.Entry::getKey,
+                tfidf.compose(Map.Entry::getValue),
+                (k1, k2) -> k1,
+                HashMap::new
+            ));
+    }
+}
