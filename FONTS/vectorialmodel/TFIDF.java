e341651b93db8c9482eee4c7bbf6c3d5dd9a0f53
Marcel Claramunt Bassegoda <marcel.claramunt@estudiantat.upc.edu>
2022-11-04 11:26:20 +0100

    TFIDF now normalizes

--- FONTS/vectorialmodel/TFIDF.java
+++ FONTS/vectorialmodel/TFIDF.java
@@ -1,13 +1,28 @@
 package vectorialmodel;
 
 import helpers.Maths;
+import io.vavr.Tuple;
 import io.vavr.collection.HashMap;
 
 public class TFIDF {
 
-    public static HashMap<String, Double> computeTFIDF(HashMap<String, Integer> termFrequencies, long maxFrequency, long documentsCount) {
-        return termFrequencies.mapValues(
-            frequency -> (frequency / maxFrequency) * Maths.log2(documentsCount / maxFrequency)
+    /**
+     * 
+     * @param termFrequencies
+     * @param maxFrequency
+     * @param documentsCount
+     * @return a normalized tfidf weight vector
+     */
+    public static HashMap<String, Double> computeTFIDF(HashMap<String, Integer> termFrequencies, long maxFrequency, long documentsCount, HashMap<String, Integer> documentFrequencies) {
+        var v = termFrequencies.map((term, frequency) -> Tuple.of(
+            term, 
+            (frequency / (double) maxFrequency) * Maths.log2(documentsCount / (double) documentFrequencies.get(term).get())
+        ));
+
+        var norm = Math.sqrt(
+            v.values().map(Maths::square).sum().doubleValue()
         );
+
+        return v.mapValues(Maths.divideBy(norm)); // normalized v
     }
 }
