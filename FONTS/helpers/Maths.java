a68110611b8890059ed96deff833b3201eade514
Marcel Claramunt Bassegoda <marcel.claramunt@estudiantat.upc.edu>
2022-11-04 11:25:07 +0100

    Update Maths.java

--- FONTS/helpers/Maths.java
+++ FONTS/helpers/Maths.java
@@ -1,7 +1,22 @@
 package helpers;
 
+import java.util.function.Function;
+
 public class Maths {
     public static double log2(double x) {
         return Math.log(x) / Math.log(2);
+    }
+
+    public static double square(double x) {
+        return x*x;
+    }
+
+    public static Function<Double, Double> divideBy(double d) {
+        return x -> x / d;
+    }
+
+    public static double round(double value, int decimals) {
+        double power = Math.pow(10, decimals);
+        return Math.round(value*power)/power;
     }
 }
