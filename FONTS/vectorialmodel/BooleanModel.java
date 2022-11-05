45314236b4b1698fcce8de76e675f7d72421c487
Marcel Claramunt Bassegoda <marcel.claramunt@estudiantat.upc.edu>
2022-11-05 13:42:19 +0100

    BooleanModel comments and small revisions

--- FONTS/vectorialmodel/BooleanModel.java
+++ FONTS/vectorialmodel/BooleanModel.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Collections;
 
 import io.vavr.Tuple;
+import io.vavr.collection.Array;
 import io.vavr.collection.HashMap;
 import io.vavr.collection.HashSet;
 import io.vavr.collection.Set;
@@ -26,10 +27,22 @@ public class BooleanModel<SentenceId> {
         return new BooleanModel<SentenceId>(Index.empty());
     }
 
+    /**
+     * 
+     * @param <SentenceId>
+     * @param index
+     * @return a new BooleanModel with the given index
+     */
     public static <SentenceId> BooleanModel<SentenceId> of(Index<SentenceId> index) {
         return new BooleanModel<SentenceId>(index);
     }
 
+    /**
+     * 
+     * @param <SentenceId>
+     * @param collection
+     * @return a new BooleanModel describing the given collection of sentences
+     */
     public static <SentenceId> BooleanModel<SentenceId> of(java.util.Map<SentenceId, java.util.Collection<String>> collection) {
         return new BooleanModel<SentenceId>(
             Index.of(
@@ -45,8 +58,7 @@ public class BooleanModel<SentenceId> {
      * @return a new BooleanModel with the document inserted. If it previously existed, it is replaced
      */
     public BooleanModel<SentenceId> insert(SentenceId sentenceId, Iterable<String> content) {
-        var newIndex = this.index.insert(sentenceId, content);
-        return BooleanModel.of(newIndex);
+        return BooleanModel.of(this.index.insert(sentenceId, content));
     }
 
     /**
@@ -55,8 +67,7 @@ public class BooleanModel<SentenceId> {
      * @return a new BooleanModel with the sentenceId (and its term occurrences) removed (if it existed)
      */
     public BooleanModel<SentenceId> remove(SentenceId sentenceId) {
-        var newIndex = this.index.remove(sentenceId);
-        return BooleanModel.of(newIndex);
+        return BooleanModel.of(this.index.remove(sentenceId));
     }
 
     /**
@@ -67,8 +78,13 @@ public class BooleanModel<SentenceId> {
     public java.util.Set<SentenceId> queryTerm(String term) {
         return queryTermVavr(term).toJavaSet();
     }
-
-    private Set<SentenceId> queryTermVavr(String term) {
+    
+    /**
+     * 
+     * @param term
+     * @return a vavr Set of the sentenceIds containing term
+     */
+    public Set<SentenceId> queryTermVavr(String term) {
         return index.postingList(term).keySet();
     }
     
@@ -81,32 +97,41 @@ public class BooleanModel<SentenceId> {
         return querySetVavr(set).toJavaSet();
     }
 
+    /**
+     * @cost O(L*S), where S == set.size(), L == min{ doc frequency foreach term in set }
+     * @param set
+     * @return a vavr Set of the sentenceIds containing all the terms in set
+     */
     @SuppressWarnings("deprecation")
     public Set<SentenceId> querySetVavr(Collection<String> set) {
-        var Ds = io.vavr.collection.Array.ofAll(set).map(this::queryTermVavr);
-        var Dmin = Ds.<Integer>minBy(Set::size).getOrElse(HashSet::empty);
-        return Dmin.filter(d -> Ds.forAll(Di -> Di.contains(d)));
+        var Ds = Array.ofAll(set).map(this::queryTermVavr);
+        return Ds.minBy(Set::size) // compare all using the smallest set
+            .map(Dmin ->  Dmin.filter(d -> Ds.forAll(Di -> Di.contains(d))))
+            .getOrElse(HashSet::empty);
     }
 
     /**
+     * @cost O(L*S), where S == sequence.size(), L == min{ doc frequency foreach term in sequence }
      * @param sequence sequence of terms to search
      * @return ids of sentences containing all the terms of the sequence in order
      */
     @SuppressWarnings("deprecation") // forall is getting de-deprecated!
     public java.util.Set<SentenceId> querySequence(java.util.List<String> sequence) {
 
-        var Ds = io.vavr.collection.Array.ofAll(sequence).map(index::postingList);
+        var postingLists = Array.ofAll(sequence).map(index::postingList);
 
-        return Ds.minBy(HashMap::size).map(
-            Dmin -> Dmin.map((d, P) -> Tuple.of(d,
+        return postingLists.minBy(HashMap::size).map(
+            minList -> {
+                int i = postingLists.indexOf(minList);
+                return minList.map((d, P) -> Tuple.of(d,
                     P.filter(
-                        pos -> Ds.zipWithIndex()
-                        .forAll(t -> t._1.containsKey(d) &&
-                                     t._1.get(d).get().contains(pos + t._2 - Ds.indexOf(Dmin)))
+                        pos -> postingLists.zipWithIndex() // tuples (_1: postingList, _2: index)
+                        .forAll(t -> t._1.get(d).getOrElse(HashSet::empty).contains(pos + t._2 - i))
                     ))
                 )
                 .filterValues(HashSet::nonEmpty)
-                .keySet().toJavaSet()
+                .keySet().toJavaSet();
+            }
         )
         .getOrElse(Collections::emptySet);
     }
