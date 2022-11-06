198521f69f519209551d23be121692f4c317c1dc
Marcel Claramunt Bassegoda <marcel.claramunt@estudiantat.upc.edu>
2022-11-06 13:22:47 +0100

    Update Tokenizer.java

--- FONTS/preprocessing/Tokenizer.java
+++ FONTS/preprocessing/Tokenizer.java
@@ -1,7 +1,19 @@
 package preprocessing;
 
 public class Tokenizer {
-    public static String[] tokenize(String text) {
-        return text.split(" "); // TODO
+    /**
+     * @param sentence a sentence (doesn't contain '.', '...', etc.)
+     * @return an array of tokens of the text
+     */
+    public static String[] tokenize(String sentence) {
+        return sentence.split(" ");
+    }
+
+    /**
+     * @param text
+     * @return an array of phrases (in String form) of the text
+     */
+    public static String[] splitPhrases(String text) {
+        return text.split(".");
     }
 }
