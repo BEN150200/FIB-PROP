714229615382023b246aaf80e17c6d4d747df623
Marcel Claramunt Bassegoda <marcel.claramunt@estudiantat.upc.edu>
2022-11-08 11:22:07 +0100

    IndexingController amb Either's per excepcions

--- FONTS/domain/indexing/core/IndexingController.java
+++ FONTS/domain/indexing/core/IndexingController.java
@@ -5,25 +5,33 @@ import java.util.Map;
 import domain.indexing.booleanmodel.BooleanModel;
 import domain.indexing.booleanmodel.ExpressionTree;
 import domain.indexing.vectorial.VectorialModel;
+import io.vavr.control.Either;
 
+import java.security.InvalidParameterException;
 import java.util.HashMap;
 
 public class IndexingController<DocId, SentenceId> {
     private VectorialModel<DocId> vectorialModel;
     private BooleanModel<SentenceId> booleanModel;
-
+    
+    // TODO: return boolean of wheter inserted new or updated,
+    // or exception plus add `updateDocument` method?
     public void addDocument(DocId docId, Iterable<String> content) {
         this.vectorialModel = this.vectorialModel.insert(docId, content);
     }
-
+    
+    // TODO: return boolean of wheter inserted new or updated,
+    // or exception plus add `updateDocument` method?
     public void addSentence(SentenceId sentenceId, Iterable<String> content) {
         this.booleanModel = this.booleanModel.insert(sentenceId, content);
     }
     
+    // TODO: return boolean of whether it was removed, or exception?
     public void removeDocument(DocId docId) {
         this.vectorialModel = this.vectorialModel.remove(docId);
     }
 
+    // TODO: return boolean of whether it was removed, or exception?
     public void removeSentence(SentenceId sentenceId) {
         this.booleanModel = this.booleanModel.remove(sentenceId);
     }
@@ -32,12 +40,13 @@ public class IndexingController<DocId, SentenceId> {
         return this.vectorialModel.querySimilars(termsWeights);
     }
 
-    public HashMap<DocId, Double> querySimilarDocuments(DocId docId) {
-        return this.vectorialModel.querySimilars(docId);
+    @SuppressWarnings("deprecation")
+    public Either<Exception, HashMap<DocId, Double>> querySimilarDocuments(DocId docId) {
+        return this.vectorialModel.querySimilars(docId).toEither(() -> new InvalidParameterException("DocId " + docId + " does not exist"));
     }
 
     // TODO
-    public Iterable<SentenceId> booleanQuery(ExpressionTree expressionTree) {
-        return null;
+    public Either<Exception, Iterable<SentenceId>> booleanQuery(ExpressionTree expressionTree) {
+        return Either.left(new UnsupportedOperationException("Not yet implemented"));
     }
 }
