d114bc8847a6766e133c394133e9bfb850c5778f
Marcel Claramunt Bassegoda <marcel.claramunt@estudiantat.upc.edu>
2022-11-09 23:07:35 +0100

    Finished weightedQuery, changed Eithers to String

--- FONTS/domain/indexing/core/IndexingController.java
+++ FONTS/domain/indexing/core/IndexingController.java
@@ -2,10 +2,21 @@ package domain.indexing.core;
 
 import java.util.Map;
 
+import domain.core.Sentence;
 import domain.indexing.booleanmodel.BooleanModel;
 import domain.indexing.booleanmodel.ExpressionTree;
 import domain.indexing.vectorial.VectorialModel;
+import helpers.Maps;
+import helpers.Maths;
+import helpers.Parsing;
+import helpers.Strings;
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import io.vavr.collection.List;
+import io.vavr.collection.Stream;
 import io.vavr.control.Either;
+import io.vavr.control.Try;
+import io.vavr.control.Validation;
 
 import java.security.InvalidParameterException;
 import java.util.HashMap;
@@ -17,10 +28,20 @@ public class IndexingController<DocId, SentenceId> {
     // TODO: return boolean of wheter inserted new or updated,
     // or exception plus add `updateDocument` method?
     public IndexingController() {
-        vectorialModel = VectorialModel.empty();
-        booleanModel = BooleanModel.empty();
+        this.vectorialModel = VectorialModel.empty();
+        this.booleanModel = BooleanModel.empty();
+    }
+
+    public static <DocId, SentenceId> IndexingController<DocId, SentenceId> of(VectorialModel<DocId> vectorialModel, BooleanModel<SentenceId> booleanModel) {
+        return new IndexingController<DocId, SentenceId>(vectorialModel, booleanModel);
     }
     
+    private IndexingController(VectorialModel<DocId> vectorialModel, BooleanModel<SentenceId> booleanModel) {
+        this.vectorialModel = vectorialModel;
+        this.booleanModel = booleanModel;
+    }
+
+
     public void addDocument(DocId docId, Iterable<String> content) {
         this.vectorialModel = this.vectorialModel.insert(docId, content);
     }
@@ -46,16 +67,34 @@ public class IndexingController<DocId, SentenceId> {
     }
 
     @SuppressWarnings("deprecation")
-    public Either<Exception, HashMap<DocId, Double>> querySimilarDocuments(DocId docId) {
-        return this.vectorialModel.querySimilars(docId).toEither(() -> new InvalidParameterException("DocId " + docId + " does not exist"));
+    public Either<String, HashMap<DocId, Double>> querySimilarDocuments(DocId docId) {
+        return this.vectorialModel.querySimilars(docId).toEither("DocId " + docId + " does not exist");
     }
-
+    
     // TODO
-    public Either<Exception, Iterable<SentenceId>> booleanQuery(ExpressionTree expressionTree) {
-        return Either.left(new UnsupportedOperationException("Not yet implemented"));
+    public Either<String, Iterable<SentenceId>> booleanQuery(ExpressionTree expressionTree) {
+        return Either.left("Not yet implemented");
     }
+    
+    public Either<String, HashMap<DocId, Double>> weightedQuery(String query) {
+        var queryTerms = query.strip().split(" ");
 
-    public Either<Exception, Iterable<DocId>> weightedQuery(String query) {
-        return Either.left(new UnsupportedOperationException("Not yet implemented"));;
+        if(query.isEmpty())
+            return Either.left("Unexpected empty query");
+
+        return Either.sequence(Stream.of(queryTerms).map(Parsing::parseWeightedTerm))
+            .map(termsWeights ->
+                termsWeights.foldLeft(
+                    io.vavr.collection.HashMap.<String, Double>empty(),
+                    (queryMap, termWeight) -> queryMap.merge(
+                        io.vavr.collection.HashMap.of(termWeight._1, termWeight._2),
+                        (a, b) -> a + b
+                    )
+                )
+            )
+            .map(Maths::normalized)
+            .map(io.vavr.collection.HashMap::toJavaMap)
+            .map(vectorialModel::querySimilars)
+            .mapLeft(Strings::joinEndLine);
     }
 }
