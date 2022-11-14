33393af79c187f12943e1c4a7b49aede6a388e3e
BENisAlreadyTaken <116182274+BENisAlreadyTaken@users.noreply.github.com>
2022-11-14 21:57:55 +0100

    funciona pero compila testos

--- FONTS/compiler.bat
+++ FONTS/compiler.bat
@@ -1,5 +1,5 @@
-dir /s /B .\src\*.java > sources.txt
+dir /s /B .\*.java > sources.txt
 
-javac -d ..\EXE\ --class-path ..\lib\vavr-1.0.0-alpha-4.jar @sources.txt
+javac -d ..\EXE\ --class-path lib\vavr-1.0.0-alpha-4.jar;lib\hamcrest-core-1.3.jar;lib\junit-4.13.2.jar @sources.txt
 
-del/f "sources.txt"
+del /f "sources.txt"
