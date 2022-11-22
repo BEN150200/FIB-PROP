5f50de27c4831df4885402ee92eda085cd16e221
biel.escorsell <biel.escorsell@estudiantat.upc.edu>
2022-11-22 20:10:37 +0100

    moure llibreries

--- FONTS/src/presentation/MainCtrl.java
+++ FONTS/src/presentation/MainCtrl.java
@@ -14,6 +14,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import src.domain.core.DocumentInfo;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
