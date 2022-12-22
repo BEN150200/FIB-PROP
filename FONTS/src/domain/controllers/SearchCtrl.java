cdc6ca8d079a296e7ea9497b480f62f4d86e505a
Marcel Claramunt Bassegoda <marcel.claramunt@estudiantat.upc.edu>
2022-12-22 11:42:04 +0100

    sort by sim

--- FONTS/src/domain/controllers/SearchCtrl.java
+++ FONTS/src/domain/controllers/SearchCtrl.java
@@ -69,6 +69,8 @@ public class SearchCtrl {
                         .withSimilarity(sim)
                     ))
                     .values()
+                    .sortBy(DocumentInfo::getSimilarity)
+                    .reverse()
                     .toJavaList()
                 )
             );
